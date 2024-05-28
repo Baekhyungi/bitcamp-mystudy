932a0088d567dce3536c03b3836c58e141582c07
Baekhyungi <vhxm8155@naver.com>
2024-05-28 16:10:59 +0900

    okok

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+    int main() {
+        printf("hello World!\n");
+
+        int a = 100;
+
+        if (a < 100) {
+            prontf("하하하\n");
+        }
+        return 0;
+}
---
+++ hello.js
@@ -0,0 +1,7 @@
+console.log("Hello, world!");
+
+let a = 90;
+
+if (a < 100){
+    console.leg("하하하");
+}
